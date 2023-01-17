cef0d025e95b14bc4688238e8ec049a6e8db301e
Ismail Majed <ismail.majed@ucalgary.ca>
2023-01-17 12:46:03 -0700

    Attempting to detect the end of measurements instead of waitingpredetermined amount of time

--- KeysightN5242B.py
+++ KeysightN5242B.py
@@ -24,48 +24,55 @@ def measSparam(temp, kind):
 	print("\n")
 	print("Basic Operations using IVI Native Commands")
 
-	# Reset State
+	
 	#driver.system.factory_preset()
-	# driver.display.windows.create_or_delete(True, 1)
-	# driver.display.windows.create_or_delete(True, 2)
-	# driver.display.windows.create_or_delete(True, 3)
-	# driver.display.windows.create_or_delete(True, 4)
+	driver.display.windows.create_or_delete(True, 1)
+	driver.display.windows.create_or_delete(True, 2)
+	driver.display.windows.create_or_delete(True, 3)
+	driver.display.windows.create_or_delete(True, 4)
 
 	# # Create 1st Measurement
-	# driver.channels.add_measurement("S11", 1, 1)
-	# driver.display.windows[0].traces.feed_measurement_number(1, 1)
+	driver.channels.add_measurement("S11", 1, 1)
+	driver.display.windows[0].traces.feed_measurement_number(1, 1)
 
 	# # Create 2nd Measurement
-	# driver.channels.add_measurement("S21", 2, 1)
-	# driver.display.windows[1].traces.feed_measurement_number(2, 1)
+    driver.channels.add_measurement("S21", 2, 1)
+	driver.display.windows[1].traces.feed_measurement_number(2, 1)
 
-	# driver.channels.add_measurement("S12", 3, 1) #Create a new Measurement
-	# driver.display.windows[2].traces.feed_measurement_number(3, 1) #Feed that measurement to a new trace
+	driver.channels.add_measurement("S12", 3, 1) #Create a new Measurement
+	driver.display.windows[2].traces.feed_measurement_number(3, 1) #Feed that measurement to a new trace
 
-	# driver.channels.add_measurement("S22", 4, 1) #Create a new Measurement
-	# driver.display.windows[3].traces.feed_measurement_number(4, 1) #Feed that measurement to a new trace
+	driver.channels.add_measurement("S22", 4, 1) #Create a new Measurement
+	driver.display.windows[3].traces.feed_measurement_number(4, 1) #Feed that measurement to a new trace
 
+	driver.status.standard_event.enable_register = inst.StatusOperationFlags.AVERAGING_SUMMARY	
+	driver.status.service_request_enable_register = keysight_ktna.StatusByteFlags.OPERATION_SUMMARY
+	driver.status.clear()
 	#Trigger Channel
 
-	# driver.channels[0].averaging.enabled = 1
-	# driver.channels[0].averaging.factor = 128
-	# driver.channels[0].standard_stimulus.sweep.frequency.start = 100E6 #Initialize state
-	# driver.channels[0].standard_stimulus.sweep.frequency.stop = 2E9
-	# driver.channels[0].averaging.if_bandwidth = 1000
+	driver.channels[0].averaging.enabled = 1
+	driver.channels[0].averaging.factor = 128
+	driver.channels[0].standard_stimulus.sweep.frequency.start = 100E6 #Initialize state
+	driver.channels[0].standard_stimulus.sweep.frequency.stop = 2E9
+	driver.channels[0].averaging.if_bandwidth = 1000
 
-	# driver.channels[0].standard_stimulus.sweep.trigger_mode = inst.SweepTriggerMode.CONTINUOUS
-	# #time.sleep(150)
+	driver.channels[0].standard_stimulus.sweep.trigger_mode = inst.SweepTriggerMode.CONTINUOUS
+	while 1:
+		time.sleep(1)
+		status_byte = driver.status.read_status_byte_register()
+		print(status_byte)
+		pass
 		
 
 
-	#Measure and save S11
-	#data_Result1 = driver.channels[0].measurements[0].query_snp_data_for_specified_ports("1")
-	#driver.system.wait_for_operation_complete(datetime.timedelta(seconds = 10))
-	driver.memory.save_trace_data("./NoiseParams/NoiseParam{}k.csv".format(temp),inst.keysight_ktna.TraceDataFileType.CSV_FORMATTED_DATA,inst.keysight_ktna.DataScope.TRACE,inst.keysight_ktna.DataFormat.RI,1)	
-	#driver.channels[0].measurements[0].save_snp_data("1 2", "./S-params/S-ParamMeas{}k.s2p".format(temp))
-	#driver.memory.upload_data("./S-params/S-ParamMeas{}k.s2p".format(temp),"C:/Users/labuser/Documents/LabSetup/Results/{}/S-ParamMeas{}k_{}.s2p".format(kind,temp,kind))
-	driver.memory.upload_data("./NoiseParams/NoiseParam{}k.csv".format(temp), "C:/Users/labuser/Documents/LabSetup/Results/{}/NoiseParam{}k_{}.csv".format(kind,temp,kind))
-	driver.system.wait_for_operation_complete(datetime.timedelta(seconds = 100))
+	# Measure and save S11
+	# data_Result1 = driver.channels[0].measurements[0].query_snp_data_for_specified_ports("1")
+	# driver.system.wait_for_operation_complete(datetime.timedelta(seconds = 10))
+	# #driver.memory.save_trace_data("./NoiseParams/NoiseParam{}k.csv".format(temp),inst.keysight_ktna.TraceDataFileType.CSV_FORMATTED_DATA,inst.keysight_ktna.DataScope.TRACE,inst.keysight_ktna.DataFormat.RI,1)	
+	# driver.channels[0].measurements[0].save_snp_data("1 2", "./S-params/S-ParamMeas{}k.s2p".format(temp))
+	# driver.memory.upload_data("./S-params/S-ParamMeas{}k.s2p".format(temp),"C:/Users/labuser/Documents/LabSetup/Results/{}/S-ParamMeas{}k_{}.s2p".format(kind,temp,kind))
+	# #driver.memory.upload_data("./NoiseParams/NoiseParam{}k.csv".format(temp), "C:/Users/labuser/Documents/LabSetup/Results/{}/NoiseParam{}k_{}.csv".format(kind,temp,kind))
+	# driver.system.wait_for_operation_complete(datetime.timedelta(seconds = 100))
 
 
 
